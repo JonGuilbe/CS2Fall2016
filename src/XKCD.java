39b8fa2ee1161ce04a0a34de4179b3a8ed3a7b51
JonGuilbe <jguilbe@knights.ucf.edu>
2016-10-17 08:45:26 -0400

    Worked through answer a bit

--- src/XKCD.java
+++ src/XKCD.java
@@ -15,21 +15,27 @@ public class XKCD{
         if(length > 20){
             System.out.println("Mostly Harmless");
         }
-        else(){
-
+        else{
+            Integer[] word = new Integer[length];
+            answer(word, length, 0, 0);
         }
         
     } 
     
-    public static void answer(Integer[] word, int length, int i){
-        if(i == length)
+    public static void answer(Integer[] word, int length, int i, int sum){
+        if(i == length && sum == 42)
             printWord(word);
         //this sum won't work, will it?
-        int sum = 0;
         for(int j = 26; j > 2; j--){
-            if(sum + j <= 42)
+            if(j == 25 || j == 21 || j == 15 || j == 9 || j == 5)
+                j++;
+            if(sum + j <= 42){
+                word[i] = j;
+                sum += j;
+                answer(word, length, i + 1, sum);
+                sum -= j;
+            }
         }
-
     }
 
     public static void printWord(Integer[] word){
