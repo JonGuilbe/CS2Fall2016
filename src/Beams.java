a8cd7306d30308709df9ab7f93760fb5f246f75f
JonGuilbe <jguilbe@knights.ucf.edu>
2016-10-20 21:27:44 -0400

    Initial beams code

---
+++ src/Beams.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Jonathan on 10/20/2016.
+ */
+
+import java.util.*;
+public class Beams {
+    public static void main(String[] args){
+        Scanner stdin = new Scanner(System.in);
+        String lasers = stdin.next();
+        int length =  lasers.length();
+
+
+
+    }
+
+}
