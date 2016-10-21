309ec981222d10df70f94c8f4b07722feb05c302
JonGuilbe <jguilbe@knights.ucf.edu>
2016-10-21 00:53:05 -0400

    Trying to find O(n) algorithm. Try finding an answer 1 letter at a time. Every time you add a letter, it either doesn't pair with the one to the left/around it or it does? Test this tomorrow morning

--- src/Beams.java
+++ src/Beams.java
@@ -3,13 +3,39 @@
  */
 
 import java.util.*;
+import java.util.stream.Collectors;
+
 public class Beams {
     public static void main(String[] args){
         Scanner stdin = new Scanner(System.in);
         String lasers = stdin.next();
-        int length =  lasers.length();
+        if(lasers.length() < 2 )
+            System.out.println("0");
+        else {
+            int sum = 0;
+            List<Character> list = lasers.chars().mapToObj((i) -> Character.valueOf((char) i)).collect(Collectors.toList());
 
+            for (int i = 0; i < list.size() - 1; i++) {
+                if(i == 0)
+                    if(list.get(i) == list.get(list.size()-1)){
+                        list.remove(i);
+                        list.remove(list.size()-1);
+                        sum++;
+                        i--;
+                }
+                else{
+                        if(list.get(i) == list.get(i + 1)){
+                            list.remove(i);
+                            list.remove(i + 1);
+                            sum++;
+                            i--;
+                        }
+                    }
 
+            }
+            sum+= list.size() / 2;
+            System.out.println(sum);
+        }
 
     }
 
