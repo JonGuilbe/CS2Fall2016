9779336a1d22265d1f74012e3a77a49199f0bc82
JonGuilbe <jguilbe@knights.ucf.edu>
2016-10-22 00:06:29 -0400

    Trying to get the string builder to work, so far i'm pretty darn close

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -2,37 +2,127 @@ import java.util.*;
 //Split array into 2   164    3
 public class Palindrome{
     static Integer[][] memo;
+    static String[][] path;
+    static String word;
     public static void main(String[] args) {
         Scanner stdin = new Scanner(System.in);
-        String word = stdin.next();
+        word = stdin.next();
         if (word.length() == 1)
             //Prevents any potential sneaky test cases
             System.out.println(1);
-        else if(word.length() == 2){
-            System.out.println(0);
-        }
         else {
             memo = new Integer[word.length()][word.length()];
-            System.out.println(rec(0,word.length()-1,word));
+            path = new String[word.length()][word.length()];
+            System.out.println(rec(0,word.length()-1));
+            System.out.println("Top row of the memo table: " + Arrays.toString(memo[0]));
+            System.out.println("Top row of the string table: " + Arrays.toString(path[0]));
+            System.out.println("2Top row of the memo table: " + Arrays.toString(memo[1]));
+            System.out.println("2Top row of the string table: " + Arrays.toString(path[1]));
+            printString();
+            //backtrack(word);
         }
     }
 
-    public static int rec(int start, int end, String word){
+    public static int rec(int start, int end){
         if(memo[start][end] != null)
             return memo[start][end];
         if(start == end){
             //System.out.println("Start is " + start + " and end is " + end);
-            return 1;
+            memo[start][end] = 1;
+            path[start][end] = Character.toString(word.charAt(start));
+            //System.out.println(path[start][end]);
+            return memo[start][end];
         }
         else if(start > end) return 0;
         else if(word.charAt(start) == word.charAt(end)){
-            memo[start][end] = 2 + rec(start+1, end - 1, word);
+            memo[start][end] = 2 + rec(start+1, end - 1);
+            path[start][end] = path[start+1][end-1] + Character.toString(word.charAt(start));
+            //System.out.println(path[start][end]);
             return memo[start][end];
         }
-        else{
-            memo[start][end] =  Math.max(rec(start, end-1,word), rec(start+1,end,word));
+        else{ //Broke something here...
+            memo[start][end] =  Math.max(rec(start, end-1), rec(start+1,end));
+            if (path[start + 1][end] != null || path[start][end-1] != null) {
+                int rightLength;
+                int leftLength;
+                if(path[start + 1][end] != null){
+                    rightLength = path[start+1][end].length();}
+                else{
+                    rightLength = 0;}
+                if(path[start][end-1] != null){
+                    leftLength = path[start][end-1].length();}
+                else{
+                    leftLength = 0;}
+                if (rightLength == leftLength) {
+                    if (path[start + 1][end] != null && new StringBuilder(path[start + 1][end]).reverse().toString().compareTo(new StringBuilder(path[start][end - 1]).reverse().toString()) == 1) {
+                        path[start][end] = path[start + 1][end];
+                    }
+                    else
+                        path[start][end] = path[start][end-1];
+                    }
+            }
             return memo[start][end];}
     }
+
+    public static void printString(){
+        int wordLength = memo.length - 1;
+        int maxLength = memo[0][wordLength];
+        int i = 0;
+        int j = wordLength;
+        ArrayList<String> palindromeList = new ArrayList<>();
+        while(true){
+            if(memo[i][j] != null && memo[i][j] == maxLength){
+                palindromeList.add(buildString(i,j, maxLength));
+                j--;
+            }
+            else{
+                if(j == wordLength)
+                    break;
+                i++;
+                j = wordLength;
+            }
+        }
+        Collections.sort(palindromeList);
+        System.out.println(palindromeList);
+    }
+
+    public static String buildString(int i, int j, int maxLength){
+        if(maxLength % 2 == 0){
+            return new StringBuilder(path[i][j]).reverse().toString() + path[i][j];
+        }
+        else
+            return new StringBuilder(path[i][j].substring(1)).reverse().toString() + path[i][j];
+    }
+
+    /*public static void backtrack(String word){
+        char[] palindromeWord = new char[memo[0][word.length()-1]];
+        int index = palindromeWord.length - 1;
+        int length = index;
+        for(int j = memo.length-1, i = 0; i < memo.length && j > 0;){
+            if(memo[i][j] == 1){
+                palindromeWord[index] = word.charAt(j);
+                break;
+            }
+            else if(memo[i][j] == 2 + memo[i+1][j-1]){
+                palindromeWord[index] = word.charAt(j);
+                palindromeWord[length - index] = word.charAt(j);
+                index--;
+                i++;
+                j--;
+
+            }
+            else{
+                if(memo[i][j-1] == memo[i][j]){
+                    j--;
+                }
+                else
+                    i++;
+            }
+        }
+        System.out.println(Arrays.toString(palindromeWord));
+    }  */
+
+
     /*
     public static int rec(int i, int j)
    {
@@ -54,7 +144,7 @@ public class Palindrome{
       }
 
       if (left.charAt(i) == right.charAt(j))
-      {
+      {5
          int result3 = 1+rec(i+1, j+1);
          if (result3 > result)
          {
