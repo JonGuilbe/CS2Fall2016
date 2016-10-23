af7dc1d152a447430f329b94f65b48aa83cae2c3
JonGuilbe <jguilbe@knights.ucf.edu>
2016-10-23 15:44:23 -0400

    STICKS IS DONE AND SO AM I

--- src/NeoNewSticks.java
+++ src/NeoNewSticks.java
@@ -4,49 +4,60 @@ import java.util.Scanner;
  * Created by reneg on 10/22/2016.
  */
 public class NeoNewSticks {
-    static Integer memo[][];
+    static Long memo[][];
     static int[] notchLocations;
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner stdin = new Scanner(System.in);
         int notches = stdin.nextInt();
         int stickLength = stdin.nextInt();
-        notchLocations = new int[notches+1];
-        for (int i = 0; i < notches; i++) {
+        notchLocations = new int[notches + 2];
+        notchLocations[0] = 0;
+        for (int i = 1; i <= notches; i++) {
             notchLocations[i] = stdin.nextInt();
         }
-        memo = new Integer[stickLength+1][stickLength+1];
+        notchLocations[notches+1] = stickLength;
+        memo = new Long[stickLength + 1][stickLength + 1];
         int currentLength = stickLength;
         boolean broken[] = new boolean[notchLocations.length];
 
-        System.out.println(answer(0, stickLength, broken));
+        System.out.println(answer(0, stickLength, 1));
     }
 
-    static int answer(int i, int j, boolean[] broken){
-        if(i==j)
-            return 0;
-        if(i<0 || j < 0)
-            return 0;
-        if(memo[i][j] != null){
+    public static long answer(int i, int j,int k) {
+        if (memo[i][j] != null) {
             return memo[i][j];
         }
+        if (i == j || i + 1 == j)
+            return 0;
+        if(notchLocations[k] >= j){
+            System.out.println("It worked!");
+            return 0;}
 
-        int bestCost = Integer.MAX_VALUE;
+        long bestCost = Integer.MAX_VALUE;
 
+        for (int index = k; index < notchLocations.length-1; index++) {
+            System.out.println("index is " + index + " k is " + k);
+            if (notchLocations[index] < i || notchLocations[index] > j)
+                break;
 
-        for(int k=0; k<notchLocations.length; k++){
-            if(!broken[k]){
-                int A = notchLocations[k] - i;
-                int B = j - notchLocations[k];
-                if(A < 0 || B < 0)
-                    break;
-                broken[k] = true;
+            int A = notchLocations[index] - i;
+            int B = j - notchLocations[index];
+            long currentCost;
+
                 System.out.println("A is " + A + " B is " + B);
-                int currentCost = A * B + answer(A,notchLocations[k], broken) + answer(notchLocations[k], B, broken);
-                //broken[k] = false;
+                System.out.println("Calling rec("+i+","+notchLocations[index]+") + ("+notchLocations[index]+","+j+")");
+            if(A <= 0 || B <= 0){
+                System.out.println("We shouldn't be here...");
+                break;}
+
+                currentCost = (A * B) + answer(i, notchLocations[index], k) + answer(notchLocations[index], j, k+1);
+                System.out.println("Current cost is " + currentCost);
                 bestCost = Math.min(bestCost, currentCost);
-            }
         }
-        memo[i][j] = bestCost;
-        return bestCost;
-    }
+
+            memo[i][j] = bestCost;
+            return bestCost;
+        }
+
 }
